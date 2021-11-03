5f953ba85d187c83c5f98162b3da0ad33e604be7
Techedemic <hendri@techedemic.com>
2021-11-03 10:52:52 +0200

    reset project - take 2

--- index.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: 'local.env' })
-
-const PORT = process.env.PORT;
-const API_KEY = process.env.API_KEY;
-
-console.log(`Running on port: ${PORT}`)
-console.log(`Using API KEY: ${API_KEY}`)
